2a77f90039d3b1359db161a287c799f9d74e614c
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-30 19:48:38 +0530

    allow all frontend to make request

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json()); // Parse JSON requests
 
 const corsOptions = {
-  origin: 'http://localhost:3000', // Change to your frontend URL
+  origin: '*', // Change to your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
